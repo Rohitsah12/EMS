d044f133b467219d703057976605c2867875f141
Rohit Kumar Sah <rohit.sah@op.iitg.ac.in>
2025-10-30 19:29:02 +0530

    feat: add HR role validation to employee registration route

--- backend/src/api/routes/auth.route.ts
+++ backend/src/api/routes/auth.route.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { authController } from '../controllers/auth.controller.js';
-import { requireAuth } from '../../middleware/auth.middleware.js';
+import { isHR, requireAuth } from '../../middleware/auth.middleware.js';
 
 const router = Router();
 
-router.post('/register', authController.registerEmployee);
+router.post('/register',requireAuth ,isHR,authController.registerEmployee);
 router.post('/login', authController.loginEmployee);
 router.post('/refresh', authController.refreshAccessToken);
 router.post('/logout', requireAuth, authController.logoutEmployee);
